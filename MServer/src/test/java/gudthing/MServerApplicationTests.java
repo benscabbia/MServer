c33cbdc0dea5a4651a6a36528f616dafee449909
Benjamin Scabbia <benscabbia@gmail.com>
2016-03-27 17:26:11 +0100

    first commit

---
+++ MServer/src/test/java/gudthing/MServerApplicationTests.java
@@ -0,0 +1,18 @@
+package gudthing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MServerApplication.class)
+@WebAppConfiguration
+public class MServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
