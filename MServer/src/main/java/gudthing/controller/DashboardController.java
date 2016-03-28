dd5c1cbd70003a44330efc805147ef987d8d8342
Benjamin Scabbia <benscabbia@gmail.com>
2016-03-28 21:35:05 +0100

    -added fragments for pages --header fragment --footer fragment --navbar fragment (WIP) --scripts fragment (WIP)

--- MServer/src/main/java/gudthing/controller/DashboardController.java
+++ MServer/src/main/java/gudthing/controller/DashboardController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
     @RequestMapping("/")
     String home() {
-        return "dashboard";
+        return "index";
     }
 }
