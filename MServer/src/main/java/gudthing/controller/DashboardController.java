f4227d6ba99e5c67293b26e0bd7ed1957fd24463
Benjamin Scabbia <benscabbia@gmail.com>
2016-03-28 16:18:26 +0100

    -Added bootstrap + jquery to project

---
+++ MServer/src/main/java/gudthing/controller/DashboardController.java
@@ -0,0 +1,16 @@
+package gudthing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Ben on 28/03/2016.
+ */
+
+@Controller
+public class DashboardController {
+    @RequestMapping("/")
+    String home() {
+        return "dashboard";
+    }
+}
