9b5839005d1f611576c6f779b53a0f728fddcfa4
Benjamin Scabbia <benscabbia@gmail.com>
2016-03-30 16:14:14 +0100

    -restructured a number of files -added dashboard page

---
+++ MServer/src/main/java/gudthing/controllers/ClientController.java
@@ -0,0 +1,10 @@
+package gudthing.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Ben on 30/03/2016.
+ */
+@Controller
+public class ClientController {
+}
---
+++ MServer/src/main/java/gudthing/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package gudthing.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Ben on 28/03/2016.
+ */
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method= RequestMethod.GET)
+    String index() {
+        return "index";
+    }
+
+
+    @RequestMapping("/clients")
+    String showClient(){return "clients"; }
+
+}
